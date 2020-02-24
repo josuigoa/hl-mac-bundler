919b480606fd18da17ab0971948788bdec56ed2e
Josu Igoa <jigoa@indaba.lks.es>
2020-02-24 13:57:04 +0100

    first working version

---
+++ Main.hx
@@ -0,0 +1,95 @@
+package;
+
+// import tink.Cli;
+// import tink.cli.*;
+import sys.FileSystem;
+import sys.io.Process;
+using haxe.io.Path;
+using StringTools;
+
+class Main {
+    
+    static var HDLL_LIBRARIES : Array<String> = [
+        "fmt",
+        "ssl",
+        "openal",
+        "uv",
+        "ui",
+        "sqlite",
+        "sdl"
+    ];
+    
+    static var EXCLUDED_DYLIBS : Array<String> = [
+        "libSystem",
+        "libz",
+        "/System/Library",
+        "Framework",
+        "/usr/lib/",
+        "@executable_path/"
+    ];
+    
+    static function isExcluded(lib:String) {
+        if (lib.indexOf('.dylib') == -1)
+            return true;
+        for (el in EXCLUDED_DYLIBS) {
+            if (lib.indexOf(el) != -1) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    static inline function runProcess(cmd:String, args:Array<String>) {
+        trace('running process: $cmd ${args.join(" ")}');
+        return new Process(cmd, args);
+    }
+
+    static inline function runProcessAndCheckError(cmd:String, args:Array<String>) {
+        var p = runProcess(cmd, args);
+        var perr = p.stderr.readAll().toString();
+        if (perr != null && perr != '') {
+            trace('${cmd}.err: ${perr}');
+        }
+        p.close();
+    }
+    
+	static public function main() {
+        var executablePath = Sys.args()[0];
+        if (executablePath == null)
+            Sys.exit(1);
+        
+        trace('executablePath: $executablePath');
+        if (!executablePath.endsWith('/')) {
+            executablePath += '/';
+        }
+        var librariesRelativeToExec = '$executablePath';
+        var p, pout, perr, depString, libPath, deps, depPath, depFile;
+        var regex = ~/.+?(?=dylib)/;
+        for (lib in HDLL_LIBRARIES) {
+            libPath = '$librariesRelativeToExec${lib}.hdll';
+            if (!FileSystem.exists(libPath))
+                continue;
+            
+            p = runProcess('otool', ['-L', libPath]);
+            pout = p.stdout.readAll();
+            p.close();
+            deps = pout.toString().split('\n');
+            for (d in deps) {
+                if (isExcluded(d))
+                    continue;
+                
+                depString = d.toString().trim();
+                if (regex.match(depString)) {
+                    depPath = new Path(regex.matched(0) + 'dylib');
+                } else {
+                    trace('$depString not matched with the RegExp');
+                    continue;
+                }
+
+                depFile = '${depPath.file}.${depPath.ext}';
+                runProcessAndCheckError('cp', [depPath.toString(), '$librariesRelativeToExec$depFile']);
+                runProcessAndCheckError('install_name_tool', ['-change', '${depPath.toString()}', '@executable_path/$depFile', '$libPath']);
+            }
+        }
+	}
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# HashLink OSX bundler
+
+Since the HashLink VM is not very portable in OSX systems. The HL executalbe is looking for the libraries in the instalation paths even if the library is in the same folder. For example, HL will look for `libSDL2-2.0.0.dylib` in the `/usr/local/opt/sdl2/lib/` folder, where it is installed.
+
+# What does this script?
+
+* Read the libraries needed by the `hdll` files that are close to the executable
+* Copy to that folder all the `dylib` files needed.
+* Update the `hdll` file to look for the libraries in the current folder.
+
+# Limitations
+
+This version only works if all the files are in the same folder, the executable and the hdll. It's working fine for me but I understand someone may want to get the libraries in another folder.
+
+# Usage
+
+I've used it in this way.
+
+* Compile `main.hl``
+* Copy (manually) the needed `hdll` files in the same folder.
+* Run this command `haxe --run Main /path/to/main/file`.
+* (Optional) Package the application
+  * Create a shell script to execute the application (e.g. `./hl main.hl`)
+  * Package the folder with [Platypus](https://sveinbjorn.org/platypus)
